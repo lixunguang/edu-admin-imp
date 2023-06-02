4685ec1c461f01d011e0f7bcd7a587accac7e7ad
lixunguang <29397924@qq.com>
2023-06-02 16:58:40 +0800

    后台更新前端文件

---
+++ bin/dist/assets/index.a622d4c9.js
@@ -0,0 +1,1 @@
+import{d as o,C as r,B as a}from"./arco.572a1b40.js";import{f as s,h as n}from"./vue.adf857d5.js";import"./chart.f83c6245.js";const d=o({__name:"index",setup(p){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(a(),r("div"))}});export{d as default};
